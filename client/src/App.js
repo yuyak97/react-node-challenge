f8a649334073397ce16b32a18e5d8fcf4783f8d9
Yuya Kayama <yuya.kayama97@gmail.com>
2021-07-17 00:16:12 +0900

    add: created Question1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
-import Index from './pages'
+import Index from './pages/Index'
+import Question1 from './pages/Question1'
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Route path='/' component={Index} />
+			<Route exact path='/' component={Index} />
+			<Route exact path='/question1' component={Question1} />
 		</BrowserRouter>
 	)
 }
