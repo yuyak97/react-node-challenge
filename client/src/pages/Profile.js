cfd29a1380668a7f6004d38db9d3ca44b6c93081
Yuya Kayama <yuya.kayama97@gmail.com>
2021-07-21 01:39:26 +0900

    add: added redirect to login page if user doesnt have jwt

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import {Button} from '@material-ui/core'
+import {Redirect} from 'react-router-dom'
 
 const Profile = () => {
 	const [profile, setProfile] = useState({})
+	const [redirect, setRedirect] = useState(false)
 
 	const getLoginUser = async () => {
 		try {
 			const {data} = await axios.get('http://localhost:4000/api/users/profile')
 			// console.log(data)
 			setProfile(data)
-		} catch (e) {}
+		} catch (err) {
+			console.log(err)
+			setRedirect(!redirect)
+		}
 	}
 
 	useEffect(() => {
 		getLoginUser()
 	}, [])
+
+	if (redirect) {
+		return <Redirect to={'/login'} />
+	}
 
 	return (
 		<div>
